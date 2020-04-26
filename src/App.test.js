034294f720e27df1fce2e2dc611d166362cae761
Julie <j49411@gmail.com>
2020-04-26 11:31:14 +0100

    feat: app renders correctly

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,28 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from 'react'
+import { shallow, configure } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+import App from './App'
+
+
+configure({ adapter: new Adapter() })
+
+
+describe('App', () => {
+
+  let wrapper
+
+  beforeEach(() => {
+    wrapper = shallow(<App />)
+  })
+
+  it('should render correctly', () => {
+    expect(wrapper.exists()).toBe(true)
+  })
+
+  it('should render a div', () => {
+    expect(wrapper.find('.app-div')).toBeDefined()
+    expect(wrapper.find('.app-div').length).toBe(1)
+  })
+
+})
--- src/setupTests.js
+++ src/setupTests.js
@@ -5,6 +5,6 @@
 import '@testing-library/jest-dom/extend-expect';
 
 import { configure } from 'enzyme'
-import Adapter from 'enzyme-adapter-react-16';
+import Adapter from 'enzyme-adapter-react-16'
 
 configure({ adapter: new Adapter() })
